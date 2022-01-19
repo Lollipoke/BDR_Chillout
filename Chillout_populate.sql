6efac53b57645bd9f9cfac9e169eb175a7645c8f
Melissa Gehring <melissagehringstx@gmail.com>
2022-01-19 11:36:39 +0100

    Update chillout_populate

--- Chillout_populate.sql
+++ Chillout_populate.sql
@@ -13,7 +13,7 @@ INSERT INTO Boisson (nom, contenance, disponibilité) VALUES ('Nestea citron', 5
 INSERT INTO Boisson (nom, contenance, disponibilité) VALUES ('Nestea pêche', 500, true);
 INSERT INTO Boisson (nom, contenance, disponibilité) VALUES ('Sprite', 330, true);
 
-/* - Bière - */ 
+/* - Bière - */
 
 INSERT INTO Boisson (nom, contenance, disponibilité) VALUES ('Swaf', 330, true);
 INSERT INTO Boisson (nom, contenance, disponibilité) VALUES ('Farmer blanche', 500, true);
@@ -47,15 +47,15 @@ INSERT INTO BoissonSoft VALUES (4);
 /* - Bières - */
 
 INSERT INTO BoissonAlcoolisée VALUES (5, 4.8, 16, 'Suisse', 'Vaud');
-INSERT INTO BoissonAlcoolisée VALUES (6, 4,8, 16, 'Suisse', 'Berne');
+INSERT INTO BoissonAlcoolisée VALUES (6, 4.8, 16, 'Suisse', 'Vaud');
 INSERT INTO BoissonAlcoolisée VALUES (7, 5.4, 16, 'Ecosse', 'Highland');
 INSERT INTO BoissonAlcoolisée VALUES (8, 6.1, 18, 'Suisse', 'Valais');
 INSERT INTO BoissonAlcoolisée VALUES (9, 4.8, 'Suisse', 'Fribourg');
 INSERT INTO BoissonAlcoolisée VALUES (10, 5.0, 'Suisse', 'Vaud');
 INSERT INTO BoissonAlcoolisée VALUES (11, 8.0, 'Belgique', 'Namur');
 INSERT INTO BoissonAlcoolisée VALUES (12, 6.1, 'Pays-Bas', 'Hollande');
-/*add by yaya*/
-INSERT INTO BoissonAlcoolisée VALUES (13, 4.8, 16, 'Irlande', 'Dublin');
+INSERT INTO BoissonAlcoolisée VALUES (13, 8.8, 'Irlande', 'Galway');
+
 /* - Vin - */
 
 INSERT INTO BoissonAlcoolisée VALUES (14, 10.1, 'Suisse', 'Vaud');
@@ -74,7 +74,8 @@ INSERT INTO Bière VALUES (8, 'Blonde');
 INSERT INTO Bière VALUES (9, 'Blonde');
 INSERT INTO Bière VALUES (10, 'Blonde');
 INSERT INTO Bière VALUES (11, 'IPA');
-INSERT INTO Bière VALUES (12, 'Stout');
+INSERT INTO Bière VALUES (12, 'Blonde');
+INSERT INTO Bière VALUES (13, 'Stout');
 
 
 /*================ Vin ====================*/
@@ -103,7 +104,7 @@ INSERT INTO Personne (nom, prénom, dateNaissance, dateArrivée, actif) VALUES (
 /* - Staff - */
 
 INSERT INTO Personne (nom, prénom, dateNaissance, dateArrivée, actif) VALUES ('Martel', 'Laurène', '2001-09-13', '2021-11-03', true);
-INSERT INTO Personne (nom, prénom, dateNaissance, dateArrivée, actif) VALUES ('Lavalée', 'Thibault', '2001-09-13', '2021-11-03', true);
+INSERT INTO Personne (nom, prénom, dateNaissance, dateArrivée, actif) VALUES ('Lavalée', 'Thibault', '1998-12-14', '2020-06-07', true);
 INSERT INTO Personne (nom, prénom, dateNaissance, dateArrivée, actif) VALUES ('Lauzier', 'Laetitia', '1997-05-12', '2021-06-07', true);
 INSERT INTO Personne (nom, prénom, dateNaissance, dateArrivée, actif) VALUES ('Leroy', 'Arnaud', '2002-05-25', '2019-08-14', false);
 
@@ -117,7 +118,7 @@ INSERT INTO Filière VALUES ('TIC');
 INSERT INTO Filière VALUES ('TIN');
 
 
-/*================ Membre Filière ====================*/
+/*================ Membre ====================*/
 
 INSERT INTO Membre VALUES (1, 'COMEM+');
 INSERT INTO Membre VALUES (2, 'TIC');
@@ -141,31 +142,152 @@ INSERT INTO Staff VALUES (7);
 
 
 /*================ Evaluation ====================*/
-INSERT INTO Evaluation(1, 1, 4, '2021-03-13')
-INSERT INTO Evaluation(1, 2, 4, '2021-03-13')
-INSERT INTO Evaluation(2, 2, 5, '2021-03-13')
-INSERT INTO Evaluation(1, 3, 4, '2018-07-11')
-INSERT INTO Evaluation(1, 4, 4, '2019-04-11')
+/* TODO check les dates d'évaluations avec dateArrivée des Membres */
+INSERT INTO Evaluation(1, 1, 4, '2021-03-13');
+INSERT INTO Evaluation(1, 2, 4, '2021-03-13');
+INSERT INTO Evaluation(5, 2, 5, '2021-03-13');
+INSERT INTO Evaluation(1, 3, 4, '2018-07-11');
+INSERT INTO Evaluation(1, 4, 4, '2019-04-11');
+INSERT INTO Evaluation(5, 4, 5, '2021-03-13');
+INSERT INTO Evaluation(1, 5, 4, '2018-09-01');
+/*INSERT INTO Evaluation(1, 6, 4, '2019-04-10');*/ /* Membre actif qui n'a pas noté */
 
 
 /*================ Fournisseur ====================*/
 
-INSERT INTO Fournisseur VALUES ('Ammstein', 'Rue des Champignons',  34, , ,);
+INSERT INTO Fournisseur VALUES ('Ammstein', 'Rue des Champignons',  34, 1400, 'Yverdon-les-Bains', 4.9);
+INSERT INTO Fournisseur VALUES ('Landi', 'Impasse de la bonne affaire', 17, 1400, 'Yverdon-les-Bains', 0.0);
+INSERT INTO Fournisseur VALUES ('Alloboissons', 'Route du Bois',  3, 1753, 'Matran', 7.9);
+INSERT INTO Fournisseur VALUES ('Aligro', 'Avenue de la Concorde',  6, 1022, 'Chavannes-près-Renens', 4.9);
+INSERT INTO Fournisseur VALUES ('Use Less Eco', 'Chemin du port', 15, 1400, 'Yverdon-les-Bains', 2.9);
 
 
 /*================ Boisson Fournisseur ====================*/
 
-
-/*================ Stock ====================*/
-
-
-/*================ Stock Chillout ====================*/
-
-
-/*================ Stock Fournisseur ====================*/
+INSERT INTO Boisson_Fournisseur VALUES (1, 'Ammstein', 1.0);
+INSERT INTO Boisson_Fournisseur VALUES (2, 'Ammstein', 1.2);
+INSERT INTO Boisson_Fournisseur VALUES (3, 'Ammstein', 1.2);
+INSERT INTO Boisson_Fournisseur VALUES (4, 'Ammstein', 1.4);
+INSERT INTO Boisson_Fournisseur VALUES (5, 'Ammstein', 3.0);
+INSERT INTO Boisson_Fournisseur VALUES (6, 'Ammstein', 3.2);
+INSERT INTO Boisson_Fournisseur VALUES (7, 'Ammstein', 2.0);
+INSERT INTO Boisson_Fournisseur VALUES (8, 'Ammstein', 4.8);
+INSERT INTO Boisson_Fournisseur VALUES (9, 'Ammstein', 4.8);
+INSERT INTO Boisson_Fournisseur VALUES (10, 'Ammstein', 1.9);
+INSERT INTO Boisson_Fournisseur VALUES (11, 'Ammstein', 2.7);
+INSERT INTO Boisson_Fournisseur VALUES (12, 'Ammstein', 2.8);
+INSERT INTO Boisson_Fournisseur VALUES (13, 'Ammstein', 3.4);
+INSERT INTO Boisson_Fournisseur VALUES (14, 'Ammstein', 5.7);
+INSERT INTO Boisson_Fournisseur VALUES (15, 'Ammstein', 2.1);
+INSERT INTO Boisson_Fournisseur VALUES (16, 'Ammstein', 3.0);
+INSERT INTO Boisson_Fournisseur VALUES (17, 'Ammstein', 14.0);
+INSERT INTO Boisson_Fournisseur VALUES (18, 'Ammstein', 10.0);
+INSERT INTO Boisson_Fournisseur VALUES (1, 'Landi', 1.0);
+INSERT INTO Boisson_Fournisseur VALUES (3, 'Landi', 1.3);
+INSERT INTO Boisson_Fournisseur VALUES (5, 'Landi', 3.3);
+INSERT INTO Boisson_Fournisseur VALUES (7, 'Landi', 1.5);
+INSERT INTO Boisson_Fournisseur VALUES (9, 'Landi', 5.0);
+INSERT INTO Boisson_Fournisseur VALUES (11, 'Landi', 2.7);
+INSERT INTO Boisson_Fournisseur VALUES (13, 'Landi', 3.0);
+INSERT INTO Boisson_Fournisseur VALUES (15, 'Landi', 2.2);
+INSERT INTO Boisson_Fournisseur VALUES (17, 'Landi', 12.0);
+INSERT INTO Boisson_Fournisseur VALUES (1, 'Alloboissons', 0.9);
+INSERT INTO Boisson_Fournisseur VALUES (2, 'Alloboissons', 1.4);
+INSERT INTO Boisson_Fournisseur VALUES (3, 'Alloboissons', 1.2);
+INSERT INTO Boisson_Fournisseur VALUES (4, 'Alloboissons', 1.7);
+INSERT INTO Boisson_Fournisseur VALUES (5, 'Alloboissons', 2.0);
+INSERT INTO Boisson_Fournisseur VALUES (6, 'Alloboissons', 3.7);
+INSERT INTO Boisson_Fournisseur VALUES (7, 'Alloboissons', 2.1);
+INSERT INTO Boisson_Fournisseur VALUES (8, 'Alloboissons', 5.2);
+INSERT INTO Boisson_Fournisseur VALUES (9, 'Alloboissons', 5.1);
+INSERT INTO Boisson_Fournisseur VALUES (1, 'Aligro', 1.1);
+INSERT INTO Boisson_Fournisseur VALUES (5, 'Aligro', 2.8);
+INSERT INTO Boisson_Fournisseur VALUES (10, 'Aligro', 1.3);
+INSERT INTO Boisson_Fournisseur VALUES (11, 'Aligro', 2.2);
+INSERT INTO Boisson_Fournisseur VALUES (15, 'Aligro', 2.0);
 
 
 /*================ Commande ====================*/
 
+/* - Fournisseur - */
+
+INSERT INTO Commande (dateHeure, idPersonne) VALUES ('12.01.2022', 10);
+INSERT INTO Commande (dateHeure, idPersonne) VALUES ('01.01.2022', 11);
+INSERT INTO Commande (dateHeure, idPersonne) VALUES ('31.12.2021', 12);
+INSERT INTO Commande (dateHeure, idPersonne) VALUES ('21.12.2021', 6);
+
+/* - Client - */
+
+
+
+/*================ Stock ====================*/
+
+INSERT INTO Stock VALUES (1, '31.02.2023');
+INSERT INTO Stock VALUES (2, '31.02.2023');
+INSERT INTO Stock VALUES (2, '31.12.2021');
+INSERT INTO Stock VALUES (3, '12.01.2022');
+INSERT INTO Stock VALUES (4, '31.02.2023');
+INSERT INTO Stock VALUES (5, '12.01.2023');
+INSERT INTO Stock VALUES (5, '12.01.2022');
+INSERT INTO Stock VALUES (5, '31.12.2021');
+INSERT INTO Stock VALUES (6, '12.01.2023');
+INSERT INTO Stock VALUES (7, '31.02.2023');
+INSERT INTO Stock VALUES (8, '31.02.2023');
+INSERT INTO Stock VALUES (9, '31.02.2023');
+/*INSERT INTO Stock VALUES (10, '31.02.2023');
+INSERT INTO Stock VALUES (11, '31.02.2023');
+INSERT INTO Stock VALUES (12, '31.02.2023');*/ /* pas dispo */
+INSERT INTO Stock VALUES (13, '31.02.2023');
+INSERT INTO Stock VALUES (14, '31.02.2023');
+INSERT INTO Stock VALUES (15, '31.02.2023');
+INSERT INTO Stock VALUES (16, '31.02.2023');
+INSERT INTO Stock VALUES (17, '31.02.2023');
+INSERT INTO Stock VALUES (18, '31.12.2021');
+
+/*================ Stock Chillout ====================*/
+/* TODO faire attention à seulement insérer des couples idBoisson, date qui sont
+dans Stock */
+INSERT INTO StockChillout VALUES (1, '12.01.2022', 1.5);
+INSERT INTO StockChillout VALUES (1, '31.02.2023', 2.0);
+INSERT INTO StockChillout VALUES (2, '31.02.2023', 1.5);
+INSERT INTO StockChillout VALUES (4, '31.02.2023', 1.6);
+INSERT INTO StockChillout VALUES (5, '12.01.2023', 4.2);
+INSERT INTO StockChillout VALUES (5, '12.01.2022', 3.7);
+INSERT INTO StockChillout VALUES (5, '31.12.2021', 3.2);
+INSERT INTO StockChillout VALUES (7, '31.02.2023', 2.3);
+
+/*================ Stock Fournisseur ====================*/
+/* TODO vérifier que le fournisseur fournit la bière */
+INSERT INTO StockFournisseur VALUES (1, '31.02.2023', 10, '');
+INSERT INTO StockFournisseur VALUES (2, '31.02.2023', 10, '');
+INSERT INTO StockFournisseur VALUES (2, '31.12.2021', 10);
+INSERT INTO StockFournisseur VALUES (3, '12.01.2022', 10);
+INSERT INTO StockFournisseur VALUES (4, '31.02.2023', 10);
+INSERT INTO StockFournisseur VALUES (5, '12.01.2023', 10);
+INSERT INTO StockFournisseur VALUES (5, '12.01.2022', 10);
+INSERT INTO StockFournisseur VALUES (5, '31.12.2021', 10);
+INSERT INTO StockFournisseur VALUES (6, '12.01.2023', 10);
+INSERT INTO StockFournisseur VALUES (7, '31.02.2023', 10);
+INSERT INTO StockFournisseur VALUES (8, '31.02.2023', 10);
+INSERT INTO StockFournisseur VALUES (9, '31.02.2023', 10);
+/*INSERT INTO StockFournisseur VALUES (10, '31.02.2023', 10);
+INSERT INTO StockFournisseur VALUES (11, '31.02.2023', 10);
+INSERT INTO StockFournisseur VALUES (12, '31.02.2023', 10);*/ /* pas dispo */
+INSERT INTO StockFournisseur VALUES (13, '31.02.2023', 10);
+INSERT INTO StockFournisseur VALUES (14, '31.02.2023', 10);
+INSERT INTO StockFournisseur VALUES (15, '31.02.2023', 10);
+INSERT INTO StockFournisseur VALUES (16, '31.02.2023', 10);
+INSERT INTO StockFournisseur VALUES (17, '31.02.2023', 10);
+INSERT INTO StockFournisseur VALUES (18, '31.12.2021', 10);
+
 
 /*================ Commande Stock ====================*/
+
+INSERT INTO Commande_Stock VALUES (1, '12.01.2022', 10);
+INSERT INTO Commande_Stock VALUES (2, '31.02.2023', 11);
+INSERT INTO Commande_Stock VALUES (3, '31.02.2023', 12);
+INSERT INTO Commande_Stock VALUES (4, '31.02.2023', 6);
+INSERT INTO Commande_Stock VALUES (4, '12.01.2023', 6);
+INSERT INTO Commande_Stock VALUES (4, '12.01.2022', 6);
+INSERT INTO Commande_Stock VALUES (4, '31.12.2021', 6);
+INSERT INTO Commande_Stock VALUES (4, '31.02.2023', 6);
