9e0df617d3b3cd6bac8b0ee5a14f5275ba9ad04a
Alen Bijelic <alen.bijelic@heig-vd.ch>
2022-01-25 14:20:59 +0100

    Check Personne role when ordering

--- Chillout_triggers_commande.sql
+++ Chillout_triggers_commande.sql
@@ -19,3 +19,29 @@ AFTER INSERT OR UPDATE
 ON Commande
 FOR EACH ROW
 EXECUTE FUNCTION function_check_dateHeure_commande();
+
+/*=============================== Check person role when ordering in any Stock TRIGGER ==========================*/
+CREATE OR REPLACE FUNCTION function_check_role_commande() RETURNS TRIGGER
+LANGUAGE plpgsql
+AS
+$BODY$
+    BEGIN
+        IF (new.commandeFournisseur = true) THEN
+            IF EXISTS (SELECT 1 FROM Staff INNER JOIN Personne ON id = idPersonne WHERE new.idPersonne = idPersonne AND actif = TRUE) THEN
+                RETURN NEW;
+            ELSE
+                RAISE EXCEPTION 'Cannot create a supplier order while being a member';
+            END IF;
+        ELSEIF EXISTS (SELECT 1 FROM Membre INNER JOIN Personne ON id = idPersonne WHERE new.idPersonne = idPersonne AND actif = TRUE) THEN
+                RETURN NEW;
+            ELSE
+                RAISE EXCEPTION 'Cannot create a client order while being a staff';
+        END IF;
+    END;
+$BODY$;
+
+CREATE OR REPLACE TRIGGER check_person_role_before_insert_or_update_Commande
+AFTER INSERT OR UPDATE
+ON Commande
+FOR EACH ROW
+EXECUTE FUNCTION function_check_role_commande();
